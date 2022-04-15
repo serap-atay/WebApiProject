c7462a95dd8ed2b5737903677d9ba0b307d78d75
serap-atay <75393492+serap-atay@users.noreply.github.com>
2022-04-15 19:49:24 +0300

    Interfaces added

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity, bool isSaveLater = false);
+        Task AddAsync(IEnumerable<T> entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+        Task RemoveRange(IEnumerable<T> entity);
+    }
+}
